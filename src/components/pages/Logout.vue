53a2cb7a61a6d32e6106492cc0ab83f4af975c95
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-10-18 13:49:14 +0200

    Define logout component

---
+++ src/components/pages/Logout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { logout } from '../../services/auth.js'
+</script>
+
+<template>
+    <h2>Sei sicuro di voler uscire?</h2>
+    <button type="button" @click.prevent="async () => await logout()">Logout</button>
+</template>
+
+<style scoped>
+
+</style>
