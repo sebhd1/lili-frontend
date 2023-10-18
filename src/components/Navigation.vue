d4ba4d3c96d468e3475d7e47b0b4772b97a3f6f5
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-10-18 13:49:23 +0200

    Fixed basic logic in navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,19 @@
 <script setup>
+    import { useAppStore } from '../stores/app.js';
+
+    const store = useAppStore();
 
 </script>
 
 <template>
     <nav>
-        <RouterLink :to="{name: 'login'}">Login</RouterLink>
-        <RouterLink :to="{name: 'services.index'}">Servizi</RouterLink>
+        <div v-if="store.userLoggedIn">
+            <RouterLink  :to="{name: 'services.index'}">Servizi</RouterLink>
+            <RouterLink :to="{name: 'logout'}">Logout</RouterLink>
+        </div>
+        <div v-else>
+            <RouterLink :to="{name: 'login'}">Login</RouterLink>
+        </div>
     </nav>
 
 </template>
