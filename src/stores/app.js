bf36b21482adcbfff7ed4fa73bb241e20485239a
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-11-13 18:32:56 +0100

    defined store for theme

--- src/stores/app.js
+++ src/stores/app.js
@@ -14,6 +14,7 @@ function loadUserFromStorage() {
 
 export const useAppStore = defineStore('app', () => {
     const user = ref(loadUserFromStorage());
+    const theme = ref('dark');
     const enums = reactive({});
 
     const userLoggedIn = computed(() => {
